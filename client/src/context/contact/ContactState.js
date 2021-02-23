e244586424dce15ece3b38cfe6e4aba92eede380
Greg Samuelson <grssam@gmail.com>
2021-02-22 16:08:35 -0800

    UI complete but not hooked up to mongo

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import { useReducer } from 'react'
-import uuid from 'uuid'
+import { v4 as uuid } from "uuid"
 import ContactContext from './contactContext'
 import contactReducer from './contactReducer'
 import {
@@ -36,31 +36,70 @@ const ContactState = props => {
         phone: '111-111-3333',
         type: 'professional'
       }
-    ]
+    ],
+    current: null,
+    filtered: null
   }
 
   const [state, dispatch] = useReducer(contactReducer, initialState)
 
   // Add Contact
+  const addContact = contact => {
+    contact.id = uuid()
+    dispatch({ type: ADD_CONTACT, payload: contact })
+  }
 
   // Delete Contact
+  const deleteContact = id => {
+
+    dispatch({ type: DELETE_CONTACT, payload: id })
+  }
 
   // Set Current Contact
+  const setCurrent = contact => {
+
+    dispatch({ type: SET_CURRENT, payload: contact })
+  }
 
   // Clear Current Contact
+  const clearCurrent = () => {
+
+    dispatch({ type: CLEAR_CURRENT })
+  }
 
   // Update Contact
+  const updateContact = contact => {
+
+    dispatch({ type: UPDATE_CONTACT, payload: contact })
+  }
 
   // Filter Contacts
+  const filterContacts = text => {
+
+    dispatch({ type: FILTER_CONTACTS, payload: text })
+  }
 
   // Clear Filter
+  const clearFilter = () => {
+
+    dispatch({ type: CLEAR_FILTER })
+  }
 
   return (
     <ContactContext.Provider
-    value={{
-      contacts: state.contacts
-    }}>
-      { props.children }
+      value={{
+        contacts: state.contacts,
+        current: state.current,
+        filtered: state.filtered,
+        addContact,
+        deleteContact,
+        setCurrent,
+        clearCurrent,
+        updateContact,
+        filterContacts,
+        clearFilter
+      }}>
+      { props.children}
     </ContactContext.Provider>
   )
 }
