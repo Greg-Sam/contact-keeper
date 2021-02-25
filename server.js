32be77caf1a35eb8372e79ca69f211c67f527064
Greg Samuelson <grssam@gmail.com>
2021-02-24 19:56:38 -0800

    prepare for deploy

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path')
 
-const app =  express()
+const app = express()
 
 // Connect Database
 connectDB()
@@ -9,7 +10,7 @@ connectDB()
 // Init Middleware
 app.use(express.json({ extended: false }))
 
-app.get('/', (req, res) => res.json({ msg: 'welcome to the ContactKeeper API...'}))
+app.get('/', (req, res) => res.json({ msg: 'welcome to the ContactKeeper API...' }))
 
 const PORT = process.env.PORT || 5000
 
@@ -17,5 +18,13 @@ const PORT = process.env.PORT || 5000
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
+
+// Serve Static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
